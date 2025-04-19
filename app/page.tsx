e01888362c9620622169cbcdcf4192d92797fcdf
Andy Jagoe <andy@jagoe.com>
2025-04-18 20:32:10 -0600

    update api spec and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { ArrowRight, Server, MessageSquare, Users, Bot, Code, Shield, Database } from "lucide-react"
+import { ArrowRight, Bot, Code, Shield, Database, MessageSquare, BookOpen } from "lucide-react"
 
 export default function Home() {
   return (
